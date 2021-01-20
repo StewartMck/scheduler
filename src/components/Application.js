2db839892b6a7e731702067854079b4fb146a35b
Stewart <stewart.mckinlay@gmail.com>
2021-01-20 16:19:27 -0700

    Create components and implement Axios requests

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
+import "components/DayList";
+import DayList from "components/DayList";
+import Appointment from "./Appointment";
+
+const axios = require("axios");
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Sherri Bradley",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Marilyn Robertson",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Jeremiah Washington",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      },
+    },
+  },
+];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`/api/days`)
+      .then((response) => setDays([...response.data]))
+      .catch((error) =>
+        console.log(
+          `ERROR:\nStatus: ${error.response.status}\n${error.response.data}`
+        )
+      );
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +104,9 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -20,7 +114,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((appointment) => {
+          return <Appointment key={appointment.id} {...appointment} />;
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
---
+++ src/components/DayListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/DayList.scss";
+const classNames = require("classnames");
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--full": !props.spots,
+    "day-list__item--selected": props.selected,
+  });
+
+  const formatSpots = (spots) => {
+    return spots
+      ? `${spots} spot${spots > 1 ? "s" : ""} remaining`
+      : "no spots remaining";
+  };
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
